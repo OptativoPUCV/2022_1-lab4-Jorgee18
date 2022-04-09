ce466b5dc167d9471ea077e71c88e973c02a8665
JORGEVILLARREAL <>
2022-04-09 18:28:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -92,7 +92,7 @@ HashMap * createMap(long capacity) {
 
 void eraseMap(HashMap * map,  char * key) 
 {
-  if(searchMap(map,key));
+  if(searchMap(map,key))
   {
     map->buckets[map->current]->key = NULL;
     map->size -= 1; 
